2d583b370c44c9e70df14b10f4f34a840da9e5eb
Wellyngton Marcondes <wmarcondesbr@gmail.com>
2023-05-28 19:19:09 -0300

    Gerador de gráfico

--- main.js
+++ main.js
@@ -9,24 +9,41 @@ generateGroups();
 
 document.onload = () => {
   estruturaHTML();
+  grafico(12, 2, 37, 4, 15, 6, 7);
 };
 
 const soma = () => {
-  for (let i = 0; i < grupos.length; i++) {
-    // console.log(`Grupo ${i + 1}:`);
-    grupos[i].forEach((pergunta, index) => {
-      const inteligencia = ["ling", "mat", "esp", "mus", "corp", "interp", "intra"]
-      const res = document.getElementById(`res${i + 1}${index + 1}`).value;
-      inteligencia.forEach(element => {
-        (pergunta.category == element)? (element = res):'';
-      });
-
-      console.log(
-        `Pergunta ${index + 1}: ${pergunta.question} | Categoria: ${
-          pergunta.category
-        }`
-      );
-    });
-    console.log("---------------------------");
-  }
+    let ling = 0;
+    let mat = 0;
+    let esp = 0;
+    let mus = 0;
+    let corp = 0;
+    let interp = 0;
+    let intra = 0;
+    
+    for (let i = 1; i <= 7; i++) {
+        for (let j = 1; j <= 4; j++) {
+        const res = localStorage.getItem(`res${i}${j}`);
+        if (res === "ling") {
+            ling += document.getElementById(`res${i}${j}`).value;
+        } else if (res === "mat") {
+            mat += document.getElementById(`res${i}${j}`).value;
+        } else if (res === "esp") {
+            esp += document.getElementById(`res${i}${j}`).value;
+        } else if (res === "mus") {
+            mus += document.getElementById(`res${i}${j}`).value;
+        } else if (res === "corp") {
+            corp += document.getElementById(`res${i}${j}`).value;
+        } else if (res === "interp") {
+            interp += document.getElementById(`res${i}${j}`).value;
+        } else if (res === "intra") {
+            intra += document.getElementById(`res${i}${j}`).value;
+        }
+        }
+    }
+    
+    grafico(ling, mat, esp, mus, corp, interp, intra);
 };
+
+const btn = document.getElementById("btn");
+btn.addEventListener("click", soma);
