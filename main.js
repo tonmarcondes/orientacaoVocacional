a864550232ebefbf7584689233d515cf8f7dfb19
Wellyngton Marcondes <wmarcondesbr@gmail.com>
2023-05-28 21:12:18 -0300

    FIsnish

--- main.js
+++ main.js
@@ -1,49 +1,38 @@
 import { perguntas } from "./scripts/banco.js";
 import { generateGroups } from "./scripts/embaralha.js";
-import { estruturaHTML } from "./scripts/func.js";
-import { grafico } from "./scripts/grafico.js";
+import { bt1, bt2, bt3, bt4, bt5, bt6, bt7, result } from "./scripts/func.js";
 
-localStorage.clear();
 
 generateGroups();
 
 document.onload = () => {
+  localStorage.clear();
   estruturaHTML();
-  grafico(12, 2, 37, 4, 15, 6, 7);
 };
 
-const soma = () => {
-    let ling = 0;
-    let mat = 0;
-    let esp = 0;
-    let mus = 0;
-    let corp = 0;
-    let interp = 0;
-    let intra = 0;
-    
-    for (let i = 1; i <= 7; i++) {
-        for (let j = 1; j <= 4; j++) {
-        const res = localStorage.getItem(`res${i}${j}`);
-        if (res === "ling") {
-            ling += document.getElementById(`res${i}${j}`).value;
-        } else if (res === "mat") {
-            mat += document.getElementById(`res${i}${j}`).value;
-        } else if (res === "esp") {
-            esp += document.getElementById(`res${i}${j}`).value;
-        } else if (res === "mus") {
-            mus += document.getElementById(`res${i}${j}`).value;
-        } else if (res === "corp") {
-            corp += document.getElementById(`res${i}${j}`).value;
-        } else if (res === "interp") {
-            interp += document.getElementById(`res${i}${j}`).value;
-        } else if (res === "intra") {
-            intra += document.getElementById(`res${i}${j}`).value;
-        }
-        }
-    }
-    
-    grafico(ling, mat, esp, mus, corp, interp, intra);
-};
+const b1 = document.getElementById("b1");
+b1.addEventListener("click", bt1);
 
-const btn = document.getElementById("btn");
-btn.addEventListener("click", soma);
+const b2 = document.getElementById("b2");
+b2.addEventListener("click", bt2);
+
+const b3 = document.getElementById("b3");
+b3.addEventListener("click", bt3);
+
+const b4 = document.getElementById("b4");
+b4.addEventListener("click", bt4);
+
+const b5 = document.getElementById("b5");
+b5.addEventListener("click", bt5);
+
+const b6 = document.getElementById("b6");
+b6.addEventListener("click", bt6);
+
+const b7 = document.getElementById("b7");
+b7.addEventListener("click", bt7);
+
+const btResult = document.getElementById("btResult");
+btResult.addEventListener("click", result);
+
+const nomeDoUsuario = document.getElementById("nomeDoUsuario");
+nomeDoUsuario.innerHTML = `Parabéns <b>${localStorage.getItem("name")}</b>, ao término de suas respostas, este é o seu resultado:`;
--- scripts/func.js
+++ scripts/func.js
@@ -1,13 +1,89 @@
-export const estruturaHTML = () => {
-  for (let i = 1; i <= 4; i++) {
-    const div = document.createElement("div");
-    console.log(`GRUPO ${i + 1}:`);
-    div.id = `div${i + 1}`;
-    div.style.backgroundColor = "#f00";
-    div.style.height = "100px";
-    div.innerHTML = `Bloco ${i + 1}`;
-    // div.classList.add("mb-3");
-    const bloco = document.getElementById(`"bloco${i}"`);
-    bloco.appendChild(div);
+import { grafico } from "./grafico.js";
+
+export const bt1 = () => {
+  localStorage.setItem("name", document.getElementById("nameInput").value);
+  const dados = document.getElementById("dados");
+  const b1 = document.getElementById("bloco1");
+  dados.style.display = "none";
+  b1.style.display = "block";
+};
+
+export const bt2 = () => {
+  const b1 = document.getElementById("bloco1");
+  const b2 = document.getElementById("bloco2");
+  b1.style.display = "none";
+  b2.style.display = "block";
+};
+
+export const bt3 = () => {
+  const b2 = document.getElementById("bloco2");
+  const b3 = document.getElementById("bloco3");
+  b2.style.display = "none";
+  b3.style.display = "block";
+};
+
+export const bt4 = () => {
+  const b3 = document.getElementById("bloco3");
+  const b4 = document.getElementById("bloco4");
+  b3.style.display = "none";
+  b4.style.display = "block";
+};
+
+export const bt5 = () => {
+  const b4 = document.getElementById("bloco4");
+  const b5 = document.getElementById("bloco5");
+  b4.style.display = "none";
+  b5.style.display = "block";
+};
+
+export const bt6 = () => {
+  const b5 = document.getElementById("bloco5");
+  const b6 = document.getElementById("bloco6");
+  b5.style.display = "none";
+  b6.style.display = "block";
+};
+
+export const bt7 = () => {
+  const b6 = document.getElementById("bloco6");
+  const b7 = document.getElementById("bloco7");
+  b6.style.display = "none";
+  b7.style.display = "block";
+};
+
+export const result = () => {
+  const b7 = document.getElementById("bloco7");
+  const result = document.getElementById("result");
+  b7.style.display = "none";
+  result.style.display = "block";
+  
+  let ling = 0;
+  let mat = 0;
+  let esp = 0;
+  let mus = 0;
+  let corp = 0;
+  let interp = 0;
+  let intra = 0;
+  
+  for (let i = 1; i <= 7; i++) {
+      for (let j = 1; j <= 4; j++) {
+      const res = localStorage.getItem(`res${i}${j}`);
+      if (res === "ling") {
+          ling += document.getElementById(`res${i}${j}`).value;
+      } else if (res === "mat") {
+          mat += document.getElementById(`res${i}${j}`).value;
+      } else if (res === "esp") {
+          esp += document.getElementById(`res${i}${j}`).value;
+      } else if (res === "mus") {
+          mus += document.getElementById(`res${i}${j}`).value;
+      } else if (res === "corp") {
+          corp += document.getElementById(`res${i}${j}`).value;
+      } else if (res === "interp") {
+          interp += document.getElementById(`res${i}${j}`).value;
+      } else if (res === "intra") {
+          intra += document.getElementById(`res${i}${j}`).value;
+      }
+      }
   }
+  
+  grafico(ling, mat, esp, mus, corp, interp, intra);
 };
